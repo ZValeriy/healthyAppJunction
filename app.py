ff882b167540ffac2c046492d943c2ee2edda293
Valeriy <valeriyzubairov@gmail.com>
2019-11-16 09:34:47 +0200

    add 2 methods in API

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
-#
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
+
+
+@app.route("/productInfo/<int:product_ean>")
+def product_info(product_ean):
+    return "Info about product"
+
+
+@app.route("/productInfoStore/<int:product_ean>/<float:lon>/<float:lat>")
+def product_info_store(product_ean, lon, lat):
+    print("PRODUCT EAN: " + str(product_ean))
+    print("LON: "+str(lon) + " LAT: "+ str(lat))
+    return "GET INFO ABOUT PRODUCT IN STORE"
+
