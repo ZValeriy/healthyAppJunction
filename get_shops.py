c328c6d8bf8dc771ec2d9c508064266a5c872be6
Valeriy <valeriyzubairov@gmail.com>
2019-11-16 10:21:34 +0200

    Add heroku

--- get_shops.py
+++ get_shops.py
@@ -1,20 +1,23 @@
 from junc_API import get_shops_in_area, get_params_for_area, get_params_for_nearest
 from app import app
+from flask import request
 
 
-@app.route("/get_shops/<lon>/<lat>")
-def get(lon, lat):
+@app.route("/get_shops")
+def get():
+    lon = request.args.get('lon')
+    lat = request.args.get('lan')
     # Trying to get shops around the area
     try:
         params = get_params_for_area(lon, lat)
-        response = self.get_shops_in_area(params)
+        response = get_shops_in_area(params)
         # If we found something
         if response:
             return {"Code": "Success", "Message": response}, 201
         # If we don`t found any shops around the area, then try to find nearest
         else:
             params = get_params_for_nearest(lon, lat)
-            response = self.get_shops_in_area(params)
+            response = get_shops_in_area(params)
             return {"Code": "Success", "Message": response}, 201 if response \
                 else {"Code": "Success", "Message": "We found nothing, good day, sir"}, 201
     except Exception as e:
---
+++ models/product.py
@@ -0,0 +1,4 @@
+class Product:
+    def __init__(self, ean, product_name):
+        self.ean = ean
+        self.name = product_name
